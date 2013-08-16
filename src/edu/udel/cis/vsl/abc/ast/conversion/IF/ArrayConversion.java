f3aa08f419c2d924167d382a6005915726d9fb3b
siegel <siegel@55792dc1-e732-4e53-a690-a7162b4b1b17>
2013-08-16 20:45:27 +0000

    Corrected bug in grammar.  Tree constructor for declarator suffixed in type names were not consistent with those in regular declarations.

--- src/edu/udel/cis/vsl/abc/ast/conversion/IF/ArrayConversion.java
+++ src/edu/udel/cis/vsl/abc/ast/conversion/IF/ArrayConversion.java
@@ -3,6 +3,13 @@ package edu.udel.cis.vsl.abc.ast.conversion.IF;
 import edu.udel.cis.vsl.abc.ast.type.IF.ArrayType;
 import edu.udel.cis.vsl.abc.ast.type.IF.PointerType;
 
+/**
+ * Implicit conversion from array type to pointer type: the first element of the
+ * array.
+ * 
+ * @author siegel
+ * 
+ */
 public interface ArrayConversion extends Conversion {
 
 	@Override
--- src/edu/udel/cis/vsl/abc/ast/conversion/IF/FunctionConversion.java
+++ src/edu/udel/cis/vsl/abc/ast/conversion/IF/FunctionConversion.java
@@ -3,6 +3,7 @@ package edu.udel.cis.vsl.abc.ast.conversion.IF;
 import edu.udel.cis.vsl.abc.ast.type.IF.FunctionType;
 import edu.udel.cis.vsl.abc.ast.type.IF.PointerType;
 
+/** Implicit conversion of function to pointer to function. */
 public interface FunctionConversion extends Conversion {
 
 	@Override
