4359641529c8d54774e47fb7af4b904c2d86b36e
siegel <siegel@55792dc1-e732-4e53-a690-a7162b4b1b17>
2013-08-17 21:48:36 +0000

    Added new message-passing primitives to civlc.h

--- trunk/text/include/civlc.h
+++ trunk/text/include/civlc.h
@@ -35,4 +35,93 @@ int $choose_int(int n);
 /* Printf, just as in standard C */
 void printf(const char * restrict format, ...);
 
+// Message passing:
+
+/* A system type for any segment of memory used as a buffer,
+ * formed using union types */
+typedef struct __buffer__ __buffer__;
+
+/* A message formed by $message_pack */ 
+typedef struct __message__ {
+  int source;
+  int dest;
+  int tag;
+  __buffer__ data;
+  int size;
+} $message;
+
+/* creates a new message, copying data from the specified buffer */ 
+$message $message_pack(int source, int dest, int tag,
+  void *data, int size);
+
+/* returns the message source */ 
+int $message_source($message * message);
+
+/* returns the message tag */
+int $message_tag($message * message);
+
+/* returns the message destination */ 
+int $message_dest($message * message);
+
+/* returns the message size */ 
+int $message_size($message * message);
+
+/* transfers message data to buf, throwing exception if message
+ * size exceeds specified size */ 
+void $message_unpack($message * message, void *buf, int size);
+
+/* an abstract datatype representing a communicator, or set
+ * of message channels between every pair of processes in a
+ * set of processes */
+typedef struct __comm__ {
+  int nprocs;
+  $proc procs[];
+} $comm;
+
+/* creates a new comm from the given sequence of processes,
+ * by allocating memory and copying the process sequence;
+ * the new comm has no messages */
+$comm $comm_create(int nprocs, $proc * procs);
+
+/* destroys the comm, deallocating whatever was allocated
+ * in its creation */ 
+void $comm_destroy($comm * comm);
+
+/* returns the number of processes associated to the comm */ 
+int $comm_nprocs($comm * comm);
+
+/* returns a pointer to the procs array in comm */ 
+$proc * $comm_procs($comm * comm);
+
+/* adds the message to the comm */
+void $comm_enqueue($comm * comm, $message * message);
+
+/* returns true iff a matching message exists in comm */
+_Bool $comm_probe($comm * comm, int source, int dest, int tag);
+
+/* finds the first matching message and returns pointer
+ * to it without modifying comm */
+$message * $comm_seek($comm * comm, int source, int dest, int tag);
+
+/* finds the first matching message, removes it from
+ * comm, and returns pointer to message */ 
+$message * $comm_dequeue($comm * comm, int source, int dest, int tag);
+
+/* returns the number of messages from source to dest stored
+ * in comm */ 
+int $comm_chan_size($comm * comm, int source, int dest);
+
+/* returns the total number of messages in the comm */ 
+int $comm_total_size($comm * comm);
+
+// message passing constants:
+
+/* Like MPI_ANY_SOURCE, can be used in probe, seek, dequeue
+ * to match a message with any source */
+#define $COMM_ANY_SOURCE -1
+
+/* Like $COMM_ANY_SOURCE above, except for tags */
+#define $COMM_ANY_TAG -2
+
+
 #endif
