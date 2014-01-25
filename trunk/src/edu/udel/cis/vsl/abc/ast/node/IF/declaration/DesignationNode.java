a730448f998b3d25d273051b63ebaa5ea2bd44e7
siegel <siegel@55792dc1-e732-4e53-a690-a7162b4b1b17>
2014-01-25 05:34:50 +0000

    Fixed bug in type checking of arguments in function calls; fixed bug in checking of assignments where arguments on pointers to qualified or unqualified versions of compatible types.  Working on compound literals/inits.  Working on mpi.h.

--- trunk/src/edu/udel/cis/vsl/abc/ast/node/IF/declaration/DesignationNode.java
+++ trunk/src/edu/udel/cis/vsl/abc/ast/node/IF/declaration/DesignationNode.java
@@ -2,6 +2,16 @@ package edu.udel.cis.vsl.abc.ast.node.IF.declaration;
 
 import edu.udel.cis.vsl.abc.ast.node.IF.SequenceNode;
 
+/**
+ * A designation node specifies a sequence of designators. Each designator is
+ * either an array designator or field designator. The sequence naviages to a
+ * point within a compound structure.
+ * 
+ * 
+ * 
+ * @author siegel
+ * 
+ */
 public interface DesignationNode extends SequenceNode<DesignatorNode> {
 
 	@Override
--- trunk/src/edu/udel/cis/vsl/abc/ast/node/common/declaration/CommonCompoundInitializerNode.java
+++ trunk/src/edu/udel/cis/vsl/abc/ast/node/common/declaration/CommonCompoundInitializerNode.java
@@ -7,20 +7,49 @@ import edu.udel.cis.vsl.abc.ast.node.IF.declaration.CompoundInitializerNode;
 import edu.udel.cis.vsl.abc.ast.node.IF.declaration.DesignationNode;
 import edu.udel.cis.vsl.abc.ast.node.IF.declaration.InitializerNode;
 import edu.udel.cis.vsl.abc.ast.node.common.CommonSequenceNode;
+import edu.udel.cis.vsl.abc.ast.type.IF.Type;
+import edu.udel.cis.vsl.abc.ast.value.IF.CompoundValue;
 import edu.udel.cis.vsl.abc.token.IF.Source;
 
 public class CommonCompoundInitializerNode extends
 		CommonSequenceNode<PairNode<DesignationNode, InitializerNode>>
 		implements CompoundInitializerNode {
 
+	private CompoundValue members;
+
+	private Type type;
+
 	public CommonCompoundInitializerNode(Source source,
 			List<PairNode<DesignationNode, InitializerNode>> childList) {
 		super(source, "CompoundInitializer", childList);
 	}
 
+	public void setMembers(CompoundValue members) {
+		this.members = members;
+	}
+
 	@Override
 	public CompoundInitializerNode copy() {
-		return new CommonCompoundInitializerNode(getSource(), childListCopy());
+		CommonCompoundInitializerNode result = new CommonCompoundInitializerNode(
+				getSource(), childListCopy());
+
+		result.setMembers(members);
+		result.setType(type);
+		return result;
+	}
+
+	@Override
+	public CompoundValue getMembers() {
+		return members;
+	}
+
+	public void setType(Type type) {
+		this.type = type;
+	}
+
+	@Override
+	public Type getType() {
+		return type;
 	}
 
 }
