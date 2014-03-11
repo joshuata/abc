b803f4f34b4dc35760efe537d96f36e359192850
zmanchun <zmanchun@55792dc1-e732-4e53-a690-a7162b4b1b17>
2014-03-11 17:40:48 +0000

    added SARL into ABC.

---
+++ src/edu/udel/cis/vsl/abc/reason/IF/Reasoner.java
@@ -0,0 +1,14 @@
+package edu.udel.cis.vsl.abc.reason.IF;
+
+import edu.udel.cis.vsl.sarl.IF.SymbolicUniverse;
+
+/**
+ * Provides interfaces for reasoning ABC expressions using SARL.
+ * 
+ * @author
+ * 
+ */
+public interface Reasoner {
+
+	SymbolicUniverse universe();
+}
