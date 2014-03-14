d204df82d9cbede08aa608ef613279a68a838dd6
siegel <siegel@55792dc1-e732-4e53-a690-a7162b4b1b17>
2014-03-14 02:48:04 +0000

    Fixed math.h $abstract headers

--- text/include/math.h
+++ text/include/math.h
@@ -1,8 +1,8 @@
 /* ABC model of math.h */
 
-$abstract double sqrt(double x);
+$abstract $contin(3) double sqrt(double x);
 
-$abstract double ceil(double x);
+$abstract $contin(3) double ceil(double x);
 
-$abstract double exp(double x);
+$abstract $contin(3) double exp(double x);
 
