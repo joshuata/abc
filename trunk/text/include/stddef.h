8d26167737d21d7878b3a34d446f45fe9aaa5154
zmanchun <zmanchun@55792dc1-e732-4e53-a690-a7162b4b1b17>
2014-03-14 16:03:11 +0000

    added stddef.h and made string.h use it.

---
+++ trunk/text/include/stddef.h
@@ -0,0 +1,5 @@
+/* The stddef .h header defines various variable types and macros. Many of these definitions also appear in other headers. */
+
+typedef unsigned long int size_t;
+
+#define NULL ((void*)0)
