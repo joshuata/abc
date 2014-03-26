2134229df9bf30d4d86261058de56fd51a32e130
zmanchun <zmanchun@55792dc1-e732-4e53-a690-a7162b4b1b17>
2014-03-26 19:18:54 +0000

    refactored header files to avoid duplication.

--- trunk/text/include/string.h
+++ trunk/text/include/string.h
@@ -1,8 +1,3 @@
 /* ABC model of string.h */
 
-#include <stddef.h>
-
-int strcmp(const char *s1, const char *s2);
-
-/* Copies a region of memory, just as in standard C */
-void memcpy(void *p, void *q, size_t size);
+#include <string-common.h>
