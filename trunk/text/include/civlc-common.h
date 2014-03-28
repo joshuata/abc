bd104062ea4a3d2dda1a8b46efdd8b302240a994
zmanchun <zmanchun@55792dc1-e732-4e53-a690-a7162b4b1b17>
2014-03-28 20:23:22 +0000

    minor correction.

--- trunk/text/include/civlc-common.h
+++ trunk/text/include/civlc-common.h
@@ -221,7 +221,7 @@ _Bool $gcomm_defined($gcomm gcomm);
 /* Returns $true iff comm points to a valid object. */
 _Bool $comm_defined($comm comm);
 
-/* Sets (*p) with the constant value presenting NULL of $proc type. */
+/* Sets (*p) with the constant value representing NULL of $proc type. */
 void $proc_null($proc *p);
 
 #endif
