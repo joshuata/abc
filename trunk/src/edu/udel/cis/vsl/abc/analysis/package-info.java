4fa4d0496545c516e0a8377237763a456f8eae27
siegel <siegel@55792dc1-e732-4e53-a690-a7162b4b1b17>
2014-05-09 12:00:25 +0000

    Added a few more package-infos.

---
+++ trunk/src/edu/udel/cis/vsl/abc/analysis/package-info.java
@@ -0,0 +1,9 @@
+/**
+ * Analysis module: provides various algorithms which
+ * analyze an AST, leaving behind information about the
+ * AST.
+ * 
+ * Entry point: {@link edu.udel.cis.vsl.abc.analysis.Analysis Analysis}
+ */
+package edu.udel.cis.vsl.abc.analysis;
+
---
+++ trunk/src/edu/udel/cis/vsl/abc/antlr2ast/package-info.java
@@ -0,0 +1,9 @@
+/**
+ * The antlr2ast package provides a translator which
+ * takes an ANTLR tree representation of a program
+ * and produces an ABC Abstract Syntax Tree.
+ * 
+ * @see edu.udel.cis.vsl.abc.antlr2ast.Antlr2AST
+ */
+package edu.udel.cis.vsl.abc.antlr2ast;
+
