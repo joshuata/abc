999f935ae2a6ebaa4cf9a2224fc64b5c53c3ada6
siegel <siegel@55792dc1-e732-4e53-a690-a7162b4b1b17>
2014-09-27 20:23:36 +0000

    Clarified documentation on Transformer.

--- src/edu/udel/cis/vsl/abc/transform/IF/Transformer.java
+++ src/edu/udel/cis/vsl/abc/transform/IF/Transformer.java
@@ -3,6 +3,39 @@ package edu.udel.cis.vsl.abc.transform.IF;
 import edu.udel.cis.vsl.abc.ast.IF.AST;
 import edu.udel.cis.vsl.abc.token.IF.SyntaxException;
 
+/**
+ * A {@link Transformer} is a tool used to transform an {@link AST} in some way,
+ * or, more precisely, it provides a method {@link #transform(AST)} which takes
+ * an {@link AST} and returns the transformed {@link AST}.
+ * 
+ * <p>
+ * The AST returned may or may not be the same object as the given AST: some
+ * transformers might modify the given AST in place, while others might return
+ * an entirely new AST with or without modifying the original one. This choice
+ * is entirely up to the individual Transformer, but should be clearly
+ * documented in the Transformer's javadoc.
+ * </p>
+ * 
+ * <p>
+ * Every Transformer should be <strong>re-useable</strong>, i.e., it should be
+ * possible to use a single transfomer object to transform multiple ASTs, as in
+ * 
+ * <pre>
+ * newAST1 = transformer.transform(ast1);
+ * newAST2 = transformer.transform(ast2); // using same transformer
+ * ...
+ * </pre>
+ * 
+ * The easiest way to ensure this is to have the transformer instantiate a new
+ * object each time {@link #transform(AST)} is invoked. The new object is
+ * responsible for carrying out the transformation for that specific invocation,
+ * it maintains all the state that is needed for that invocation, and it goes
+ * away when the invocation completes.
+ * </p>
+ * 
+ * @author siegel
+ * 
+ */
 public interface Transformer {
 
 	/**
@@ -30,10 +63,14 @@ public interface Transformer {
 	String toString();
 
 	/**
-	 * Apply a transformation to a translation unit.
+	 * Apply a transformation to a translation unit. This may or may not result
+	 * in modifications to the given AST, and may or may not return a new AST
+	 * instance (as opposed to the given one).
 	 * 
 	 * @param ast
-	 *            The abstract syntax tree being modified.
+	 *            The abstract syntax tree being transformed
+	 * @return the transformed AST, which may or may not be the same object that
+	 *         was given
 	 * @throws SyntaxException
 	 *             If it encounters an error in the AST or an some case which it
 	 *             cannot handle
