2a71565f7d7deb2e1adb0a8c6cee7ab1269bbdab
ziqing <ziqing@55792dc1-e732-4e53-a690-a7162b4b1b17>
2014-10-09 17:32:59 +0000

    a minor change of replacing child(index) nodes with private fields with a specific name

--- src/edu/udel/cis/vsl/abc/ast/node/common/statement/CommonForLoopNode.java
+++ src/edu/udel/cis/vsl/abc/ast/node/common/statement/CommonForLoopNode.java
@@ -8,22 +8,37 @@ import edu.udel.cis.vsl.abc.token.IF.Source;
 
 public class CommonForLoopNode extends CommonLoopNode implements ForLoopNode {
 
+	private ForLoopInitializerNode initializer;
+	private ExpressionNode condition;
+	private ExpressionNode incrementer;
+
 	public CommonForLoopNode(Source source, ExpressionNode condition,
 			StatementNode statement, ForLoopInitializerNode initializer,
 			ExpressionNode incrementer, ExpressionNode invariant) {
 		super(source, LoopKind.FOR, condition, statement, invariant);
 		addChild(initializer);
 		addChild(incrementer);
+		this.initializer = initializer;
+		this.condition = condition;
+		this.incrementer = incrementer;
+
 	}
 
+	// TODO: currently just assuming for loop initializer has only 2 forms: i =
+	// 0 or int i = 0.
 	@Override
 	public ForLoopInitializerNode getInitializer() {
-		return (ForLoopInitializerNode) child(3);
+		return this.initializer;
 	}
 
 	@Override
 	public ExpressionNode getIncrementer() {
-		return (ExpressionNode) child(4);
+		return (ExpressionNode) this.incrementer;
+	}
+
+	@Override
+	public ExpressionNode getCondition() {
+		return (ExpressionNode) this.condition;
 	}
 
 	@Override
@@ -37,5 +52,4 @@ public class CommonForLoopNode extends CommonLoopNode implements ForLoopNode {
 	public StatementKind statementKind() {
 		return StatementKind.LOOP;
 	}
-
 }
