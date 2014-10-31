f12438cb355a86314e775387abb3d9324268031a
zmanchun <zmanchun@55792dc1-e732-4e53-a690-a7162b4b1b17>
2014-10-31 17:20:31 +0000

    updated adder.c to be a compilable C program.

--- trunk/examples/macro/adder.c
+++ trunk/examples/macro/adder.c
@@ -1,20 +1,24 @@
-#ifdef CIVL_PROG
+#include<stdio.h>
+#ifdef _CIVL
 #include <civlc.cvh>
 $input int N;
 $assume 0 < N && N < 10;
+#else
+#define N 100
 #endif
 
 int input[N];
 int sum = 0;
 
-void main(){
-
-for(int i = 0; i < N; i++)
-  input[i] = i;
-for(int i = 0; i < N; i++)
-  sum == input[i];
-#ifdef CIVL_PROG
-$assert sum == N*(N-1)/2;
+int main(){
+  for(int i = 0; i < N; i++)
+    input[i] = i;
+  for(int i = 0; i < N; i++)
+    sum += input[i];
+#ifdef _CIVL
+  $assert sum == N*(N-1)/2;
 #endif
+  printf("N = %d, sum = %d\n", N, sum);
+  fflush(stdout);
+  return 0;
 }
-
