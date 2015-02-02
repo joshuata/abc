579b489782b08425e355832b81c717c4d7a25106
johneden <johneden@55792dc1-e732-4e53-a690-a7162b4b1b17>
2015-02-02 21:15:15 +0000

    Modified headers

--- src/include/abc/svcomp.h
+++ src/include/abc/svcomp.h
@@ -1,7 +1,8 @@
 #ifdef __SVCOMP__
 #else
 #define __SVCOMP__
-
+void __VERIFIER_error(void);
 void __VERIFIER_assume(int);
 int __VERIFIER_nondet_int(void);
+int __VERIFIER_nondet_uint(void);
 #endif
