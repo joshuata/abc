5bb663daa7c6fe671180d626eb95c4b23dc4d2c5
siegel <siegel@55792dc1-e732-4e53-a690-a7162b4b1b17>
2015-03-10 19:46:41 +0000

    Fixed bug which was causing char to not be recognized as an int type.

--- trunk/examples/c/intChar.c
+++ trunk/examples/c/intChar.c
@@ -2,9 +2,9 @@
 #include<assert.h>
 
 int main(){
-  char a[2];
+  char a;
 
-  memset(a, 0, 2);
-  for(int i=0; i<2; i++)
-    assert(a[i] == 0);
+  //memset(a, 0, 2);
+  //for(int i=0; i<2; i++)
+    assert(a == 0);
 }
