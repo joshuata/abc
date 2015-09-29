c84a09ea5306c23afa4901f61d5d30003b023e77
zmanchun <zmanchun@55792dc1-e732-4e53-a690-a7162b4b1b17>
2015-09-29 14:37:42 +0000

    added more examples

--- examples/svcomp/function.c
+++ examples/svcomp/function.c
@@ -2,32 +2,40 @@
 /*int f();
 int g();
 void h(int k);
-void fi(int x);
 */
+void fi(int x);
+
+/*
 fnoreturn(int a){
   return a*a;
-}
+}*/
+
+int add(int, int);
 
 int main() {
   //volatile int t;
+  int x;
 
-  fnoreturn(10);
+  fi(x);
+  //x=add(1,2);
+  //fnoreturn(10);
   /* f();
   g();
   //t=add(1,2);
   h(t);*/
 }
 
-/*
-int add(int x, int y){
+
+inline int add(int x, int y){
   return x+y;
 }
 
+/*
 void h(int x){
   x++;
 }
-
+*/
 inline void fi(int x){
   x++;
   return;
-  }*/
+  }
