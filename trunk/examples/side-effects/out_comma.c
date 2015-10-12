987522a52c0de18638c591895ad1dd77909051e7
zmanchun <zmanchun@55792dc1-e732-4e53-a690-a7162b4b1b17>
2015-10-12 15:54:34 +0000

    added a comma expression with two comma operators, revealing a bug of the side-effect-remover.

--- trunk/examples/side-effects/out_comma.c
+++ trunk/examples/side-effects/out_comma.c
@@ -2,17 +2,16 @@ int* f()
 {
   return (void*)0;
 }
- 
 int main()
 {
   int* p;
   int  a[10];
-  
   *&p = f();
   &a[11];
-   
   int* $sef$0 = f();
-  
   *p = *$sef$0;
   5 / 0;
+  *p = 1;
+  a[1] = 1;
+  a[2] = 2;
 }
