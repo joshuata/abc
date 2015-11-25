e429723925728378ff8608feb8cd023b9ef3006e
zmanchun <zmanchun@55792dc1-e732-4e53-a690-a7162b4b1b17>
2015-11-25 18:07:56 +0000

    fixed redefinition of NULL macro using conditional macros.

--- src/include/abc/stddef.h
+++ src/include/abc/stddef.h
@@ -5,5 +5,7 @@ Many of these definitions also appear in other headers. */
 typedef unsigned long int size_t;
 typedef signed long int ptrdiff_t;
 
+#ifndef NULL
 #define NULL ((void*)0)
+#endif
 #endif
